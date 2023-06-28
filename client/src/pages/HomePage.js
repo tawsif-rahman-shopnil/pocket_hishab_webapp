23263d7c7e7ab4a83e4d6bb48e0311b2ca256b52
Tawsif Rahman Shopnil <shopnil@baiust.edu.bd>
2023-06-29 00:27:09 +0600

    update

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
+import { Modal } from "antd";
 import Layout from "./../components/Layout/Layout";
 
 const HomePage = () => {
+  const [showModel, setShowModel] = useState(false);
+
   return (
     <Layout>
-      <h1>Home Page</h1>
+     <div className="filters">
+      <div>range filters</div>
+      <div>
+        <button 
+        className="btn btn-primary" 
+        onClick={() => setShowModel(true)}
+        >
+          Add New
+        </button>
+      </div>
+     </div>
+     <div className="content"></div>
+      <Modal title="Add Transaction"
+       visible={showModel} onCancel={() => setShowModel(false)}>
+      
+      </Modal>     
     </Layout>
   );
 };
