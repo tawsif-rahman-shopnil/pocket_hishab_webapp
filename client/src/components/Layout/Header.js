f931f57e374fd51923db1627ab0dfbae3533c6b7
Tawsif Rahman Shopnil <shopnil@baiust.edu.bd>
2023-07-26 18:26:25 +0600

    update

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -8,6 +8,7 @@ import "../../styles/HeaderStyles.css";
 
 const Header = () => {
   const [loginUser, setLoginUser] = useState("");
+  const [showMobileMenu, setShowMobileMenu] = useState(false); // State for mobile menu
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -29,28 +30,30 @@ const Header = () => {
         <Link className="navbar-brand" to="/">
           <img src="/logo.png" alt="Logo" className="header-logo" />
         </Link>
+        {/* Mobile Menu Button */}
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={() => setShowMobileMenu(!showMobileMenu)} // Toggle mobile menu on click
         >
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div
+          className={`collapse navbar-collapse ${
+            showMobileMenu ? "show" : "" // Conditionally add 'show' class to display the mobile menu
+          }`}
+          id="navbarTogglerDemo01"
+        >
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <div className="user-info">
                 <span className="username">
                   <UserOutlined /> {loginUser && loginUser.name}
-                  </span>
-                  <span className="btn ">
-                    <button className="button" onClick={logoutHandler}>
-                  <FontAwesomeIcon icon={faSignOutAlt} />
-                </button>
+                </span>
+                <span className="btn">
+                  <button className="button" onClick={logoutHandler}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                  </button>
                 </span>
               </div>
             </li>
